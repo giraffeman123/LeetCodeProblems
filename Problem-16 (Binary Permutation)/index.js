99038a2664240bf13c38de75647d39ecbb7ad444
giraffeman123 <elliotmtz12@gmail.com>
2022-09-13 15:35:51 -0700

    change folders names

--- Problem-16 (Binary Permutation)/index.js
+++ Problem-16 (Binary Permutation)/index.js
@@ -17,41 +17,6 @@ var binaryPermutation = function(combination, n){
     }
 }
 
-app.get('/phonePermutation/:number', async(req,res) => {
-    phoneResult = [];
-    var phoneNumber = req.params.number;
-    phonePermutation("",phoneNumber,0);
-    res.status(200).json(phoneResult);
-});
-
-var phoneResult = [];
-var phoneCombinations = {
-    "2":"abc",
-    "3":"def",
-    "4":"ghi",
-    "5":"jkl",
-    "6":"mno",
-    "7":"pqrs",
-    "8":"tuv",
-    "9":"wxyz",
-};
-var phonePermutation = function(combination, phoneNumber, currentPhoneNumberIndex){
-
-    if(combination.length == phoneNumber.length)
-        phoneResult.push(combination);
-    else{
-        
-        var letterOfDigit = phoneCombinations[phoneNumber[currentPhoneNumberIndex]];
-        for(var i = 0; i < letterOfDigit.length; i++){
-            combination += letterOfDigit[i];
-            currentPhoneNumberIndex++;
-            phonePermutation(combination,phoneNumber,currentPhoneNumberIndex);                
-            combination = combination.slice(0,-1);
-            currentPhoneNumberIndex--;
-        }
-    }
-}
-
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
